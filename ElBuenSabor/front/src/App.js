2a1c92450c8cf569f99b057246d715921ea83112
PabloZavi <pablozaviok@gmail.com>
2022-09-22 22:03:50 -0300

    Agregado React-Bootstrap y NavBar

--- ElBuenSabor/front/src/App.js
+++ ElBuenSabor/front/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import NavBar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">El Buen Sabor</Link>
+          <NavBar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavBar.Brand>El Buen Sabor</NavBar.Brand>
+              </LinkContainer>
+            </Container>
+          </NavBar>
+          {/* <Link to="/">El Buen Sabor</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/producto/:id" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/producto/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center"> El Buen Sabor Footer</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
