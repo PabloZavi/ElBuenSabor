265a7492b9197c8124fceb7d3accf0698227ecab
PabloZavi <pablozaviok@gmail.com>
2022-09-28 21:13:05 -0300

    Seguimos con las variables de entorno

--- ElBuenSabor/front/src/screens/PaidOkScreen.js
+++ ElBuenSabor/front/src/screens/PaidOkScreen.js
@@ -31,7 +31,5 @@ export default function PaidOkScreen() {
   }
 
   actPedido();
-  window.location.href = process.env.HOST + '/order/' + external_reference;
-
-  
+  window.location.href = `${process.env.HOST}/order/${external_reference}`;
 }
