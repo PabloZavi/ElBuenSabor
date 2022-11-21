998ced820662bd531ff8d2fdea79a952a0bc97fe
PabloZavi <pablozaviok@gmail.com>
2022-11-20 22:23:01 -0300

    Clean not used classes

--- ElBuenSabor/front/src/index.js
+++ ElBuenSabor/front/src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,8 +18,3 @@ root.render(
     </StoreProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
